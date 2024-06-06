f48f2cb265fd1f30f8fec23bea77bec0fea651df
ivantydev <dev_ivanty@outlook.com>
2024-06-06 07:46:42 -0300

    atualizações 06/06

--- app/public/js/obrasMaisVendidasMobile.js
+++ app/public/js/obrasMaisVendidasMobile.js
@@ -1,26 +1,26 @@
-let obrasMaisVendidasslideIndex = 1;
-showSlidesEstilo(obrasMaisVendidasslideIndex);
+let obrasMaisVendidasMobileslideIndex = 1;
+showSlidesEstilo(obrasMaisVendidasMobileslideIndex);
 
 function plusSlidesEstilos(n) {
-  showSlidesEstilo(obrasMaisVendidasslideIndex += n);
+  showSlidesEstilo(obrasMaisVendidasMobileslideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlidesEstilo(obrasMaisVendidasslideIndex = n);
+  showSlidesEstilo(obrasMaisVendidasMobileslideIndex = n);
 }
 
 function showSlidesEstilo(n) {
   let i;
-  let slides = document.getElementsByClassName("obrasMaisVendidasmySlides");
-  let dots = document.getElementsByClassName("obrasMaisVendidasdot");
-  if (n > slides.length) {obrasMaisVendidasslideIndex = 1}    
-  if (n < 1) {obrasMaisVendidasslideIndex = slides.length}
+  let slides = document.getElementsByClassName("obrasMaisVendidasMobilemySlides");
+  let dots = document.getElementsByClassName("obrasMaisVendidasMobiledot");
+  if (n > slides.length) {obrasMaisVendidasMobileslideIndex = 1}    
+  if (n < 1) {obrasMaisVendidasMobileslideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[obrasMaisVendidasslideIndex-1].style.display = "block";  
-  dots[obrasMaisVendidasslideIndex-1].className += " active";
+  slides[obrasMaisVendidasMobileslideIndex-1].style.display = "block";  
+  dots[obrasMaisVendidasMobileslideIndex-1].className += " active";
 }
